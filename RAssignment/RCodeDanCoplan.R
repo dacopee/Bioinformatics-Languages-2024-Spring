e45385c70bfd31386dc08ad08967ba3974d299f7
dacopee <156939094+dacopee@users.noreply.github.com>
2024-04-15 13:36:00 -0400

    Add files via upload
    this works properly with a bar graph! For the sake of saving time, the second graph could not be computed.

--- RAssignment/RCodeDanCoplan.R
+++ RAssignment/RCodeDanCoplan.R
@@ -1,20 +1,29 @@
-#full path is C:\RIT Classes\BIO 230 Bioinformatic Lang\RCode
 library(ggplot2)
 
 print ("Hello, Welcome to Dan's R Code")
+setwd("C:\\RIT Classes\\BIO 230 Bioinformatic Lang\\RCode")
 
+#read data
 data <- read.table('gc_results.csv', header = TRUE, sep = ',')
 #View(data)
-results <- list()
+str(data)
 
-for (col_name in names(data)) {
-  anova_result <- aov(data[[col_name]] ~ 1)  # Perform ANOVA
-  results[[col_name]] <- summary(anova_result)  # Store ANOVA results
-}
+#make new stats to graph
+avg_percentages <- aggregate(GC_Percentage ~ Chromosome, data = data, FUN = function(x) {
+  c(avg = mean(x), sd = sd(x))  # Return named vector with avg and sd
+})
+avg_percentages$avg <- avg_percentages$GC_Percentage[, "avg"] # separates out the avg and sd columns for later refrence
+avg_percentages$sd <- avg_percentages$GC_Percentage[, "sd"]
 
-# Print ANOVA results
-for (col_name in names(results)) {
-  cat("ANOVA for", col_name, ":\n")
-  print(results[[col_name]])
-  cat("\n")
-}
+#ANOVA - analysis of variance (backwards)
+oneway_result <- oneway.test (GC_Percentage ~ Chromosome, data, var.equal = TRUE)
+print(oneway_result)
+
+#print error bar plots
+ggplot(avg_percentages, aes(x = Chromosome, y = avg)) +
+  geom_bar(stat = "identity", fill = "skyblue", alpha = 0.7) +
+  geom_errorbar(aes(ymin = avg - sd, ymax = avg + sd),width = 0.4) +
+    labs(x = "Chromosome", y = "Average GC Percentage", title = "Error Bar Average GC Percentage by Chromosome with Error Bars") +
+  theme_minimal()
+
+
